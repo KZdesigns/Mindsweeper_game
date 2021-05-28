cdb5b6219740d2012d315bf0e849da8aa23d808f
Kyle Zimmerman <kzdesignsios@gmail.com>
2021-05-27 21:23:44 -0700

    getter methods for tile class defined

--- tile.rb
+++ tile.rb
@@ -1,4 +1,35 @@
 class Tile
+    DELTAS = [
+        [-1, -1],
+        [-1,  0],
+        [-1,  1],
+        [ 0, -1],
+        [ 0,  1],
+        [ 1, -1],
+        [ 1,  0],
+        [ 1,  1]
+    ].freeze
+
+    attr_reader :position
+
+    def initialize(board, position)
+        @board, @position = board, position
+        @bombed, @explored, @flagged = false, false, false
+    end
+
+    def bombed?
+        @bombed
+    end
+
+    def explored?
+        @explored
+    end
+
+    def flagged?
+        @flagged
+    end
+
+    
 
   
 end
